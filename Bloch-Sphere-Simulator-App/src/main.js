200da34677ae3744e507927647c5b28c94685352
dashBuresz <csaba.szijarto03@gmail.com>
2025-11-20 08:59:47 +0100

    implentation started, rotating a vector needs to be figured out, readme.md updated, now it actually contains important info related to the project.

--- Bloch-Sphere-Simulator-App/src/main.js
+++ Bloch-Sphere-Simulator-App/src/main.js
@@ -1,24 +1,43 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from  "https://cdn.jsdelivr.net/npm/three@0.163.0/build/three.module.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.set(2,1.2,2);
+camera.lookAt(0,0,0);
+
+
+const geometry = new THREE.SphereGeometry(1, 40, 40);
+const material = new THREE.MeshBasicMaterial({color: 0x00aaff, transparent:true, opacity: 0.18, wireframe: true});
+const blochSphere = new THREE.Mesh(geometry, material);
+const contRotation = 0.002;
+scene.add(blochSphere);
+
+const axes = new THREE.AxesHelper(2);
+scene.add(axes);
+
+const direction = new THREE.Vector3(0,0,1);
+const origin = new THREE.Vector3(0,0,0);
+const length = 1;
+const color = 0xff0000;
+const blochVector = new THREE.ArrowHelper(direction.normalize(), origin, length, color);
+scene.add(blochVector);
+
+
+//functions from here
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  //blochSphere.rotation.y += contRotation;
+  //axes.rotation.y += contRotation;
+  //blochVector.setY += contRotation;
+}
+animate();
